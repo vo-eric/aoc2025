ab15659197997039737e59f3eaf41d8572f15d9b
Eric Vo <vo.eric@gmail.com>
2026-01-12 14:46:50 -0500

    add 6.2

--- day6/day6.js
+++ day6/day6.js
@@ -3,6 +3,51 @@ const { zstdCompress } = require('zlib');
 var input = fs.readFileSync('input.txt', 'utf8');
 var parsedInput = input.split(/\r?\n/);
 
+const part2 = (lines) => {
+  let result = 0;
+
+  const operations = lines[lines.length - 1].trim().split(/\s+/);
+  console.log(operations);
+  let currentOperation = operations.pop();
+  let currentResult = currentOperation === '+' ? 0 : 1;
+  const values = lines.slice(0, lines.length - 1);
+
+  for (let i = values[0].length - 1; i >= 0; i--) {
+    let vals = [];
+
+    for (let j = 0; j < values.length; j++) {
+      vals.push(values[j][i]);
+    }
+
+    if (vals.every((val) => val === ' ')) {
+      currentOperation = operations.pop();
+      result += currentResult;
+      currentResult = currentOperation === '+' ? 0 : 1;
+      continue;
+    }
+
+    const valToOperate = Number(
+      vals
+        .map((val) => {
+          return val === '0' ? '0' : val;
+        })
+        .join('')
+    );
+
+    console.log('before', currentResult, valToOperate);
+    if (currentOperation === '+') {
+      currentResult += valToOperate;
+    } else {
+      currentResult *= valToOperate;
+    }
+
+    console.log('current', currentResult);
+  }
+
+  result += currentResult;
+  return result;
+};
+
 const part1 = (lines) => {
   let result = 0;
 
@@ -29,4 +74,4 @@ const part1 = (lines) => {
   return result;
 };
 
-console.log(part1(parsedInput));
+console.log(part2(parsedInput));
