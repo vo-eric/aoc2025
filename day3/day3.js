9791e38d776151ce983f0295dec6332023ac7845
Eric Vo <vo.eric@gmail.com>
2026-01-20 14:16:22 -0500

    add 3.2

--- day3/day3.js
+++ day3/day3.js
@@ -7,45 +7,34 @@ const day1 = (lines) => {
   let result = 0;
 
   for (const line of lines) {
-    const charge = getHighestVolage(line);
+    const charge = getHighestVolage(line, 0, 12, 0);
     result += charge;
   }
 
   return result;
 };
 
-const getHighestVolage = (bank) => {
-  /*
-  scan through the line except for the last number and get 
-    the highest number with index
-  
-  do a second scan from the index and find the greatest number
+const getHighestVolage = (bank, current, remaining, prevIndex) => {
+  if (current.toString().length === 12) {
+    return current;
+  }
 
-  return the two numbers
-  */
+  let highest = -Infinity;
+  let index = prevIndex;
 
-  let first = [-Infinity, null];
-  let second = [-Infinity, null];
-
-  for (let i = 0; i < bank.length - 1; i++) {
-    const num = Number(bank[i]);
-
-    if (num > first[0]) {
-      first[0] = num;
-      first[1] = i;
+  for (let i = prevIndex; i <= bank.length - remaining; i++) {
+    if (Number(bank[i]) > highest) {
+      highest = Number(bank[i]);
+      index = i;
     }
   }
 
-  for (let i = first[1] + 1; i < bank.length; i++) {
-    const num = Number(bank[i]);
-
-    if (num > second[0]) {
-      second[0] = num;
-      second[1] = i;
-    }
-  }
-
-  return first[0] * 10 + second[0];
+  return getHighestVolage(
+    bank,
+    current * 10 + highest,
+    remaining - 1,
+    index + 1
+  );
 };
 
 console.log(day1(parsedInput));
